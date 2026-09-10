33b9dbd31a680eebfae2b6d42cf3b537b3696973
twey2 <63811507+twey2@users.noreply.github.com>
2026-09-10 06:14:39 -0400

    Fix names in downloaded scripts

--- longitudinal/00_create_harmonization_project_longi_updated.R
+++ longitudinal/00_create_harmonization_project_longi_updated.R
@@ -121,11 +121,11 @@ download.file(
   destfile = paste0(folder_name, "/03-crossreference_check_longitudinal.R"),
   mode = "wb")
 download.file(
-  url = "https://github.com/maelstrom-research/harmonization_ProPASS/raw/refs/heads/master/longitudinal/03-crossreference_check_longitudinal.R",
+  url = "https://github.com/maelstrom-research/harmonization_ProPASS/raw/refs/heads/master/longitudinal/propass_crosscheck_battery_longitudinal.R",
   destfile = paste0(folder_name, "/propass_crosscheck_battery_longitudinal.R"),
   mode = "wb")
 download.file(
-  url = "https://github.com/maelstrom-research/harmonization_ProPASS/raw/refs/heads/master/longitudinal/03-crossreference_check_longitudinal.R",
+  url = "https://github.com/maelstrom-research/harmonization_ProPASS/raw/refs/heads/master/longitudinal/propass_crosscheck_helpers_longitudinal.RR",
   destfile = paste0(folder_name, "/propass_crosscheck_helpers_longitudinal.R"),
   mode = "wb")
 
